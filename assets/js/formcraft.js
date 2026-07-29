9cb2c6316b6350bf1242501862623b5ed390ea55
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-07-29 12:11:07 +0545

    feat: load operations modules

--- assets/js/formcraft.js
+++ assets/js/formcraft.js
@@ -1,6 +1,15 @@
 (() => {
-  const script = document.createElement('script');
-  script.src = 'assets/js/admin.js';
-  script.defer = true;
-  document.head.append(script);
+  const theme = document.createElement('link');
+  theme.rel = 'stylesheet';
+  theme.href = 'assets/css/modules.css';
+  document.head.append(theme);
+
+  const admin = document.createElement('script');
+  admin.src = 'assets/js/admin.js';
+  admin.onload = () => {
+    const modules = document.createElement('script');
+    modules.src = 'assets/js/modules.js';
+    document.head.append(modules);
+  };
+  document.head.append(admin);
 })();

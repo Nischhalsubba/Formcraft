4c6c89ab725a811cabd2a7b9fb7885c8ca3b9573
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-07-29 12:24:06 +0545

    fix: keep PlanIQ direction above module styles

--- assets/js/formcraft.js
+++ assets/js/formcraft.js
@@ -1,10 +1,15 @@
 (() => {
   window.__formcraftInitialHash = location.hash.slice(1);
 
-  const theme = document.createElement('link');
-  theme.rel = 'stylesheet';
-  theme.href = 'assets/css/modules.css';
-  document.head.append(theme);
+  const modulesTheme = document.createElement('link');
+  modulesTheme.rel = 'stylesheet';
+  modulesTheme.href = 'assets/css/modules.css';
+  document.head.append(modulesTheme);
+
+  const directionTheme = document.createElement('link');
+  directionTheme.rel = 'stylesheet';
+  directionTheme.href = 'assets/css/planiq-direction.css';
+  document.head.append(directionTheme);
 
   const admin = document.createElement('script');
   admin.src = 'assets/js/admin.js';
